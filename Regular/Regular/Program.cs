81ba2981ebfa8dec66c2363b3513fb9a59a22890
WeiJingPan <466676342@qq.com>
2020-11-11 00:35:00 +0800

    Regular First Commit

---
+++ Regular/Regular/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex("Name:(\\w+)\\s*Age:(\\d{1,3})");
+            var matcher = regex.Match("Name:Aurora    Age:18");
+            if (matcher.Success)
+            {
+                var group = matcher.Groups;
+                var g1 = group[1];
+                var g2 = group[2];
+                Console.WriteLine("g1 = {0}", g1);
+                Console.WriteLine("g2 = {0}", g2);
+            }
+        }
+    }
+}
