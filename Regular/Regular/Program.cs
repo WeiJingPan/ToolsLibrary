c0dac0b00b1dece27b3000240f6d4445ad40a27e
WeiJingPan <466676342@qq.com>
2020-11-12 00:17:07 +0800

    Regular Second Commit

--- Regular/Regular/Program.cs
+++ Regular/Regular/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Regular
@@ -16,6 +18,23 @@ namespace Regular
                 var g2 = group[2];
                 Console.WriteLine("g1 = {0}", g1);
                 Console.WriteLine("g2 = {0}", g2);
+            }
+
+            
+            PrintStringSplit("二分，回溯，递归，分治".Split("[，；\\s]+"));
+            PrintStringSplit("搜索，差找，旋转，遍历".Split("[，；\\s]+"));
+            PrintStringSplit("数论，图论，逻辑，概率".Split("[，；\\s]+"));
+            
+            Console.WriteLine("二分,回溯,递归,分治".Replace("[,;\\s]+", ";"));
+            Console.WriteLine("搜索;差找;旋转;遍历".Replace("[,;\\s]+", ";"));
+            Console.WriteLine("数论 图论 逻辑 概率".Replace("[,;\\s]+", ";"));
+        }
+
+        private static void PrintStringSplit(string[] strs)
+        {
+            foreach (var str in strs)
+            {
+                Console.WriteLine(str);
             }
         }
     }
