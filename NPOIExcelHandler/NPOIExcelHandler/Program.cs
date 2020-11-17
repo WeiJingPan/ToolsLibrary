37429164a4c232206fca08cd6791118cd6b3d3b5
v_wjjwpan(潘伟靖) <v_wjjwpan@tencent.com>
2020-11-17 21:23:45 +0800

    Create Excel2003 And Operation

--- NPOIExcelHandler/NPOIExcelHandler/Program.cs
+++ NPOIExcelHandler/NPOIExcelHandler/Program.cs
@@ -12,6 +12,11 @@ namespace NPOIExcelHandler
         {
             
             var tmpPath = @"D:\C_Spaces\ToolsLibrary\GenerateArea\Excel2003.xls";
+
+            if (File.Exists(tmpPath))
+            {
+                File.Delete(tmpPath);
+            }
             
             // if (!File.Exists(tmpPath))
             // {
@@ -25,17 +30,47 @@ namespace NPOIExcelHandler
             //     workbook2003.Close();
             // }
             
-            tmpPath = @"D:\C_Spaces\ToolsLibrary\GenerateArea\Excel2007.xls";
+            // tmpPath = @"D:\C_Spaces\ToolsLibrary\GenerateArea\Excel2007.xls";
+            //
+            // var workbook2007 = new XSSFWorkbook();
+            // workbook2007.CreateSheet("Sheet1");
+            // workbook2007.CreateSheet("Sheet2");
+            // workbook2007.CreateSheet("Sheet3");
+            // var fs2007 = new FileStream(tmpPath , FileMode.Create);
+            // workbook2007.Write(fs2007);
+            // fs2007.Close();
+            // workbook2007.Close();
             
-            var workbook2007 = new XSSFWorkbook();
-            workbook2007.CreateSheet("Sheet1");
-            workbook2007.CreateSheet("Sheet2");
-            workbook2007.CreateSheet("Sheet3");
-            var fs2007 = new FileStream(tmpPath , FileMode.Create);
-            workbook2007.Write(fs2007);
-            fs2007.Close();
-            workbook2007.Close();
+            var workbook2003 = new HSSFWorkbook();
+            workbook2003.CreateSheet("Sheet1");
+            var sheetOne = (HSSFSheet)workbook2003.GetSheet("Sheet1");
+            for (int i = 0; i < 10; i++)
+            {
+                sheetOne.CreateRow(i);
+            }
+
+            var sheetRow = (HSSFRow) sheetOne.GetRow(0);
+            var sheetCell = new HSSFCell[10];
+            for (int i = 0; i < 10; i++)
+            {
+                sheetCell[i] = (HSSFCell) sheetRow.CreateCell(i);
+            }
             
+            sheetCell[0].SetCellValue(true);
+            sheetCell[1].SetCellValue(0.111111);
+            sheetCell[2].SetCellValue("Excel2003");
+            sheetCell[3].SetCellValue("123456789132456798");
+
+            for (int i = 4; i < 10; i++)
+            {
+                sheetCell[i].SetCellValue(i);
+            }
+            
+            var fs = new FileStream(tmpPath, FileMode.Create);
+            workbook2003.Write(fs);
+            fs.Close();
+            workbook2003.Close();
+
         }
     }
 }
